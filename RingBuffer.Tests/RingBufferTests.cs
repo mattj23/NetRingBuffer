fdb27005363d9eadbe7a419ab132f3a8848d996c
Matt Jarvis <mattj23@gmail.com>
2021-07-22 11:54:57 -0400

    start of ring buffer

--- RingBuffer.Tests/RingBufferTests.cs
+++ RingBuffer.Tests/RingBufferTests.cs
@@ -6,9 +6,26 @@ namespace RingBuffer.Tests
     public class RingBufferTests
     {
         [Fact]
-        public void Test1()
+        public void TestEmptyBuffer()
         {
+            RingBuffer buffer = new RingBuffer(10);
 
+            Assert.Equal(0, buffer.Count);
+            Assert.Equal(10, buffer.Available);
+            Assert.Equal(10, buffer.Capacity);
         }
+
+        [Fact]
+        public void TestAddBytes()
+        {
+            var testData = new byte[] {1, 2, 3, 4};
+            var buffer = new RingBuffer(10);
+
+            buffer.Push(testData);
+
+            Assert.Equal(4, buffer.Count);
+            Assert.Equal(6, buffer.Available);
+        }
+
     }
 }
--- RingBuffer/RingBuffer.cs
+++ RingBuffer/RingBuffer.cs
@@ -1,7 +1,49 @@
-﻿namespace RingBuffer
+﻿using System;
+
+namespace RingBuffer
 {
     public class RingBuffer
     {
-        
+        private readonly byte[] _buffer;
+
+        /// <summary>
+        /// The index of the first element of data
+        /// </summary>
+        private int _start;
+
+        /// <summary>
+        /// This is the index of the element just past the last actual data
+        /// </summary>
+        private int _end;
+
+        public RingBuffer(int capacity)
+        {
+            _buffer = new byte[capacity];
+            _start = 0;
+            _end = 0;
+        }
+
+        public int Count => _end;
+
+        public int Available => _buffer.Length - Count;
+
+        public int Capacity => _buffer.Length;
+
+        public void Push(byte[] data)
+        {
+            foreach (var b in data)
+            {
+                _buffer[_end] = b;
+                _end++;
+            }
+        }
+
+        public byte[] Take()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
     }
 }
