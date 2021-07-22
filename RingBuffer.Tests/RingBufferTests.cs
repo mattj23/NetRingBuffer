887ea9799b8465a66a45667d57648ca4a84d233a
Matt Jarvis <mattj23@gmail.com>
2021-07-22 12:50:00 -0400

    basic functionality

--- RingBuffer.Tests/RingBufferTests.cs
+++ RingBuffer.Tests/RingBufferTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Xunit;
 
 namespace RingBuffer.Tests
@@ -16,7 +17,7 @@ namespace RingBuffer.Tests
         }
 
         [Fact]
-        public void TestAddBytes()
+        public void TestAddBytesAtStart()
         {
             var testData = new byte[] {1, 2, 3, 4};
             var buffer = new RingBuffer(10);
@@ -25,6 +26,70 @@ namespace RingBuffer.Tests
 
             Assert.Equal(4, buffer.Count);
             Assert.Equal(6, buffer.Available);
+            Assert.Equal(1, buffer[0]);
+            Assert.Equal(2, buffer[1]);
+            Assert.Equal(3, buffer[2]);
+            Assert.Equal(4, buffer[3]);
+        }
+
+        [Fact]
+        public void TestTakeBytesAtStart()
+        {
+            var testData = new byte[] {1, 2, 3, 4};
+            var buffer = new RingBuffer(10);
+            buffer.Push(testData);
+
+            var result = buffer.Take(4);
+
+            Assert.Equal(0, buffer.Count);
+            Assert.Equal(10, buffer.Available);
+            Assert.Equal(1, result[0]);
+            Assert.Equal(2, result[1]);
+            Assert.Equal(3, result[2]);
+            Assert.Equal(4, result[3]);
+        }
+
+        [Fact]
+        public void TestStressBuffer()
+        {
+            var testCount = 100000;
+            var sampleData = new byte[testCount];
+            var readData = new byte[0];
+            var random = new Random();
+            random.NextBytes(sampleData);
+
+            int readIndex = 0;
+            int writeIndex = 0;
+
+            var ring = new RingBuffer(50);
+
+            while (readIndex < testCount)
+            {
+                // If the buffer is empty OR it's not full and we pass a random coin flip, we will write
+                // data into it.  If the buffer is empty we have no choice but to write since a read is
+                // impossible.
+                if (ring.IsEmpty || (!ring.IsFull && random.NextDouble() > 0.5))
+                {
+                    // Write some amount  of data into it
+                    var amount = random.Next(1, ring.Available);
+                    amount = Math.Min(amount, testCount - writeIndex);
+                    var values = sampleData.Skip(writeIndex).Take(amount).ToArray();
+                    ring.Push(values);
+                    writeIndex += amount;
+                }
+
+                // Otherwise we read from the buffer and check that it matches the data that was put in
+                else
+                {
+                    var amount = random.Next(1, ring.Count);
+                    amount = Math.Min(amount, testCount - readIndex);
+                    var result = ring.Take(amount);
+                    readData = readData.Concat(result).ToArray();
+                    readIndex += amount;
+                }
+            }
+
+            Assert.Equal(sampleData, readData);
         }
 
     }
--- RingBuffer/RingBuffer.cs
+++ RingBuffer/RingBuffer.cs
@@ -2,8 +2,15 @@
 
 namespace RingBuffer
 {
+    /// <summary>
+    /// A ring buffer (or circular buffer) is a fixed array stored in memory which acts like an
+    /// infinitely advancing queue. 
+    /// </summary>
     public class RingBuffer
     {
+        /// <summary>
+        /// Internal storage array
+        /// </summary>
         private readonly byte[] _buffer;
 
         /// <summary>
@@ -16,33 +23,129 @@ namespace RingBuffer
         /// </summary>
         private int _end;
 
+        /// <summary>
+        /// Create a ring buffer with a fixed capacity
+        /// </summary>
+        /// <param name="capacity"></param>
         public RingBuffer(int capacity)
         {
-            _buffer = new byte[capacity];
+            Capacity = capacity;
+            _buffer = new byte[capacity + 1];
             _start = 0;
             _end = 0;
         }
 
-        public int Count => _end;
+        /// <summary>
+        /// Gets the total number of items currently held by the ring buffer
+        /// </summary>
+        public int Count => (_end >= _start) ? _end - _start : Capacity + 1 - _start + _end;
 
-        public int Available => _buffer.Length - Count;
+        /// <summary>
+        /// Gets the number of empty spaces left in the ring buffer
+        /// </summary>
+        public int Available => Capacity - Count;
 
-        public int Capacity => _buffer.Length;
+        /// <summary>
+        /// Gets the total capacity of the ring buffer, which was set when it was created
+        /// </summary>
+        public int Capacity { get; }
 
-        public void Push(byte[] data)
+        /// <summary>
+        /// Returns true if there are no elements in the ring buffer
+        /// </summary>
+        public bool IsEmpty => Count == 0;
+
+        /// <summary>
+        /// Returns true if there are no empty spaces in the ring buffer
+        /// </summary>
+        public bool IsFull => Available == 0;
+
+        /// <summary>
+        /// Gets (peek) or sets the element at an index in the ring without altering the number of elements stored. Accessing
+        /// a value beyond the valid range of stored elements (as shown by Count) has undefined behavior.
+        /// </summary>
+        /// <param name="index">The index of the element to access, expressed as an offset from the first valid entry in the buffer</param>
+        /// <returns></returns>
+        public byte this[int index]
         {
+            get => _buffer[RawIndexOf(index)];
+            set => _buffer[RawIndexOf(index)] = value;
+        }
+
+        /// <summary>
+        /// Pushes a number of elements onto the end of the buffer, increasing the total count of elements and
+        /// decreasing the capacity
+        /// </summary>
+        /// <param name="data"></param>
+        public void Push(ReadOnlySpan<byte> data)
+        {
+            if (data.Length > Available) 
+                throw new IndexOutOfRangeException("Attempting to add more elements to the buffer than there is available space");
+
             foreach (var b in data)
             {
                 _buffer[_end] = b;
-                _end++;
+                AdvanceEnd();
             }
         }
 
-        public byte[] Take()
+        /// <summary>
+        /// Removes a number of elements from the front of the buffer and adds them to a range, decreasing the
+        /// number of stored elements in the buffer. If the output span provided is larger than the number of elements
+        /// currently stored in the buffer an IndexOutOfRangeException will be thrown.
+        /// </summary>
+        /// <param name="output">An output Span into which the leading elements will be copied</param>
+        public void Take(Span<byte> output)
         {
-            throw new NotImplementedException();
+            if (output.Length > Count) 
+                throw new IndexOutOfRangeException("Attempting to take more elements than are in the buffer");
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                output[i] = _buffer[_start];
+                AdvanceStart();
+            }
         }
 
+        /// <summary>
+        /// Removes a number of elements from the front of the buffer and returns them as an array, decreasing the
+        /// number of elements stored in the buffer. If the requested quantity to take is larger than the number of
+        /// elements stored in the buffer an IndexOutOfRangeException will be thrown.
+        /// </summary>
+        /// <param name="quantity">Number of elements to take from the front of the buffer</param>
+        /// <returns></returns>
+        public byte[] Take(int quantity)
+        {
+            var output = new byte[quantity];
+            Take(output);
+            return output;
+        }
+
+        /// <summary>
+        /// Calculate the _buffer index that corresponds with an index offset from _start
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        private int RawIndexOf(int i)
+        {
+            return (_start + i) % (Capacity + 1);
+        }
+
+        /// <summary>
+        /// Increment the _end index by one, wrapping around to the front if necessary
+        /// </summary>
+        private void AdvanceEnd()
+        {
+            if (++_end >= _buffer.Length) _end = 0;
+        }
+
+        /// <summary>
+        /// Increment the _start index by one, wrapping around to the front if necessary
+        /// </summary>
+        private void AdvanceStart()
+        {
+            if (++_start >= _buffer.Length) _start = 0;
+        }
 
 
     }
